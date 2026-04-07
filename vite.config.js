ec7e6f81529a616f31c16dad9145b7ceda0067d4
周俊花 <3484530003@qq.com>
2026-04-08 01:58:24 +0800

    fix-最终版

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/-TicketM_System_Front/',
   server: {
     port: 8080,
     open: true
